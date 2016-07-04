7567c841a56da0e5342efb9803d5f644dc837add
Droan Malik <droanmalik@gmail.com>
2016-07-04 06:01:17 +0530

    menu options

--- app/src/main/java/me/droan/udacityshowcase/showcase/ShowcaseFragment.java
+++ app/src/main/java/me/droan/udacityshowcase/showcase/ShowcaseFragment.java
@@ -4,14 +4,21 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.List;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import me.droan.udacityshowcase.R;
 import me.droan.udacityshowcase.data.Post;
 
@@ -19,6 +26,10 @@ import me.droan.udacityshowcase.data.Post;
  * Created by drone on 04-07-2016.
  */
 public class ShowcaseFragment extends Fragment implements ShowcaseContract.View {
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.recyclerView)
+    RecyclerView recyclerView;
     private ShowcaseAdapter adapter;
     private ShowcaseContract.Presenter showcasePresenter;
 
@@ -47,11 +58,39 @@ public class ShowcaseFragment extends Fragment implements ShowcaseContract.View 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.showcase_fragment, container, false);
+        ButterKnife.bind(this, view);
+        initToolbar();
+        initRecyclerView();
+        return view;
+    }
 
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+    private void initRecyclerView() {
         recyclerView.setLayoutManager(new UnscrollableLinearLayoutManager(getActivity()));
         recyclerView.setAdapter(adapter);
-        return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
+    }
+
+    private void initToolbar() {
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.showcase_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+
     }
 
     @Override
@@ -67,6 +106,12 @@ public class ShowcaseFragment extends Fragment implements ShowcaseContract.View 
     @Override
     public void setPresenter(ShowcaseContract.Presenter presenter) {
         this.showcasePresenter = presenter;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
     }
 
     private static class UnscrollableLinearLayoutManager extends LinearLayoutManager {
